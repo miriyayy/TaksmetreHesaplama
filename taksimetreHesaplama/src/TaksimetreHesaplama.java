42ee74ce3932809da79683fb4928c9d51322df36
Miray Sude mutlu <mutlumiraysude34@gmail.com>
2023-12-17 12:08:27 +0300

    TaksmetreHesaplama

---
+++ taksimetreHesaplama/src/TaksimetreHesaplama.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class TaksimetreHesaplama {
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        int km;
+        double perKm= 2.20 , total, startPrice=10;
+
+
+        System.out.println("Mesafeyi Km cinsinden giriniz");
+        km= input.nextInt();
+        total= (km*perKm);
+        total += startPrice;
+
+        total=(total<20)? 20: total;
+        System.out.print( "Toplam Tutar : "+ total);
+
+    }
+}
